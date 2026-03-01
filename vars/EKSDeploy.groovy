d9e699c33ad4fc58dca183b69817c1acc6011956
bharathchandu302 <bharathchandu302@gmail.com>
2026-03-01 17:04:21 +0530

    EKSDeploy.groovy

---
+++ vars/EKSDeploy.groovy
@@ -0,0 +1,64 @@
+def call (Map configMap){
+    pipeline {
+    agent {
+        node {
+            label 'AGENT-1'
+            
+        }
+    }
+    environment { 
+        COURSE = 'jenkins'
+        appVersion = configMap.get("appVersion")
+        ACC_ID = "022779559954"
+        PROJECT = configMap.get("project")
+        COMPONENT = configMap.get("component")
+        deploy_to = configMap.get("deploy_to")
+        REGION = "us-east-1"
+    }
+    options {
+        timeout(time: 30, unit: 'HOURS') 
+        disableConcurrentBuilds()
+    }
+     /* parameters {
+        string(name: 'appVersion', description: 'which app version you want to deploy')
+        choice(name: 'deploy_to', choices: ['dev', 'qa', 'prod'], description: 'Pick something')
+
+    } */
+
+    stages{
+        stage('Deploy') {
+            steps {
+                script {
+                    withAWS(region:'us-east-1',credentials:'aws-creds'){
+                    sh """
+                        aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
+                        kubectl get nodes
+                        echo ${deploy_to},${appVersion}
+                    """
+
+                    }
+                }
+            }
+        }
+    }
+    
+    
+        
+    post { 
+        always { 
+            echo 'I will always say Hello again!'
+            cleanWs()
+            }
+            success {
+                echo 'i will run if success'
+            }
+            failure {
+                echo 'i will run if failure'
+            }
+            aborted {
+                echo 'pipeline is aborted'
+            }
+        }
+    }
+
+}
